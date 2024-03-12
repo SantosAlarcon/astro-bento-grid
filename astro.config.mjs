f4d317356a62347db0662bc0f7cf19824bf2a8b9
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-12 20:51:11 +0100

    Añadido componente de tarjeta de credito

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	base: '/astro-bento-grid/',
+	site: 'http://localhost:4321/astro-bento-grid/',
 	integrations: [mdx(), sitemap()],
 });

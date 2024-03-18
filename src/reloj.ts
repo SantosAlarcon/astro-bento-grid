db69595d994c1f31a36689549bf586ecfc5f5c82
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-18 13:35:06 +0100

    add: Nuevos componentes de Twitter, S.O. y móvil

--- src/reloj.ts
+++ src/reloj.ts
@@ -1,8 +1,12 @@
-const reloj = (fecha: Date): string => {
-  let hora = fecha.getHours()
-  let minutos = fecha.getMinutes()
-  let segundos = fecha.getSeconds()
-  return `${hora}:${minutos}:${segundos}`
-}
 
-export default reloj
+document.addEventListener("DOMContentLoaded", () => {
+    function reloj(fecha: Date): string {
+      let hora = fecha.getHours().toString().padStart(2, '0')
+      let minutos = fecha.getMinutes().toString().padStart(2, '0')
+      return `${hora}:${minutos}`
+    }
+  const clock = document.querySelector('#clock')
+  setInterval(() => {
+	clock!.textContent = reloj(new Date())
+  }, 1000)	
+})
